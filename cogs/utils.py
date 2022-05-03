f565eff5d6199554f6ab500089ea1904b3ace28b
ItsNeil <68216747+ItsNeil17@users.noreply.github.com>
2022-05-03 20:54:32 +0530

    Moved commands commands to tags cog #2
    Signed-off-by: ItsNeil <68216747+ItsNeil17@users.noreply.github.com>

--- cogs/utils.py
+++ cogs/utils.py
@@ -28,29 +28,5 @@ class Utils(commands.Cog):
         else:
             await ctx.send("You're not allowed to send an update!")
 
-    @commands.command()
-    async def release(self, ctx):
-        await ctx.message.delete()
-        e = discord.Embed(
-            title="Hello there,",
-            description="We are working hard to get EventsApp released into beta.\n\nWhen it is ready it will be announced in <#970282258890096654>.\n\nKind Regards,\nThe EventsApp Team",
-            color=discord.Color.dark_purple(),
-            timestamp=datetime.datetime.utcnow(),
-        )
-        e.set_footer(text=str(ctx.author), icon_url=ctx.author.avatar)
-        await ctx.send(embed=e)
-
-    @commands.command()
-    async def help(self, ctx):
-        await ctx.message.delete()
-        e = discord.Embed(
-            title="Need Help?",
-            description="Do you need help? Shoot a DM to <@970304297382313984> and we'll reply as fast as we can.\n\nKind Regards,\nThe EventsApp Team",
-            color=discord.Color.dark_purple(),
-            timestamp=datetime.datetime.utcnow(),
-        )
-        e.set_footer(text=str(ctx.author), icon_url=ctx.author.avatar)
-        await ctx.send(embed=e)
-
 def setup(bot):
     bot.add_cog(Utils(bot))
