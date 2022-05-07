a2f3cde7af2cdfb243d76c76ed0109656c82c838
ItsNeil <68216747+ItsNeil17@users.noreply.github.com>
2022-05-07 15:16:30 +0530

    Rename to StagParty
    Signed-off-by: ItsNeil <68216747+ItsNeil17@users.noreply.github.com>

--- cogs/tags.py
+++ cogs/tags.py
@@ -12,7 +12,7 @@ class Tags(commands.Cog):
         await ctx.message.delete()
         tag_embed = discord.Embed(
             title="Hello there,",
-            description="We are working hard to get EventsApp released into beta.\n\nWhen it is ready it will be announced in <#970282258890096654>.\n\nKind Regards,\nThe EventsApp Team",
+            description="We are working hard to get StagParty released into beta.\n\nWhen it is ready it will be announced in <#970282258890096654>.\n\nKind Regards,\nThe StagParty Team",
             color=discord.Color.dark_purple(),
             timestamp=datetime.datetime.utcnow(),
         )
@@ -24,7 +24,7 @@ class Tags(commands.Cog):
         await ctx.message.delete()
         tag_embed = discord.Embed(
             title="Need Help?",
-            description="Do you need help? Shoot a DM to <@970304297382313984> and we'll reply as fast as we can.\n\nKind Regards,\nThe EventsApp Team",
+            description="Do you need help? Shoot a DM to <@970304297382313984> and we'll reply as fast as we can.\n\nKind Regards,\nThe StagParty Team",
             color=discord.Color.dark_purple(),
             timestamp=datetime.datetime.utcnow(),
         )
